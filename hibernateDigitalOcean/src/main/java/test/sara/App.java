6a949871673a49b4493c5d4642c0620ef032a799
saraHH1417 <sara.afzar@gmail.com>
2023-01-26 02:58:29 +0330

    feat: creating baldung and howtodo projects

---
+++ hibernateDigitalOcean/src/main/java/test/sara/App.java
@@ -0,0 +1,13 @@
+package test.sara;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ hibernateDigitalOcean/src/main/java/test/sara/hibernate/model/Employee1.java
@@ -0,0 +1,6 @@
+package test.sara.hibernate.model;
+
+public class Employee1 {
+
+
+}
